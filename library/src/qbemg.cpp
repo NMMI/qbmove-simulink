3aea5990c34f1ccba3019dca81bedd804ffc9f2b
qbrobotics <info@qbrobotics.com>
2017-01-23 16:12:58 +0100

    Changed license to BSD 3-Clause License

--- library/src/qbemg.cpp
+++ library/src/qbemg.cpp
@@ -1,10 +1,37 @@
 // -----------------------------------------------------------------------------
-// Copyright (C)  qbrobotics. All rights reserved.
-// www.qbrobotics.com
-// -----------------------------------------------------------------------------
-// File:        qbotISS_ISS.cpp
+// BSD 3-Clause License
 //
-// Description: Communication s-function for obtaining EMG of qbHand.
+// Copyright (c) 2017, qbrobotics
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// -----------------------------------------------------------------------------
+// File:        qbemg.cpp
+//
+// Description: Communication s-function to obtain EMG measurements of qbHand.
 //              To be used with RS-485 on a Virtual COM.
 //------------------------------------------------------------------------------
 
