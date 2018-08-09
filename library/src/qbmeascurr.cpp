3c1d5c1c0f772d8986f1395c2a5af85e0cbab76b
qbrobotics_AR <alessandro.raugi@qbrobotics.com>
2018-08-09 10:32:53 +0200

    License update

--- library/src/qbmeascurr.cpp
+++ library/src/qbmeascurr.cpp
@@ -1,7 +1,7 @@
 // -----------------------------------------------------------------------------
 // BSD 3-Clause License
 //
-// Copyright (c) 2017, qbrobotics
+// Copyright (c) 2015-2018, qbrobotics
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
